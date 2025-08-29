b0f422c322a2f85da4eb62d9073e34663e17c3b6
Shishir-Roka <shishirroka616@gmail.com>
2025-08-29 10:19:02 +0545

    sign up

--- Backend/Routes/user.routes.js
+++ Backend/Routes/user.routes.js
@@ -1,17 +1,40 @@
-import express from "express"
-import db from '../db/index.js'
-import { usersTable } from "../Modules/user.model.js";
+import express from "express";
+import db from "../db/index.js";
+import { usersTable } from "../Module/user.model.js";
+import { signupPostRequestBodySchema } from "../validations/request.validation.js";
 import { eq } from "drizzle-orm";
+import {randomBytes , createHmac} from "crypto"
 
+const router = express.Router();
 
-const router = express();
+router.get("/", async (req, res) => {
+  const [users] = await db.select().from(usersTable);
+  res.json({ data: users });
+});
 
-router.post("/",async (req,res)=>{
-  const { firstName, lastName , email, password } =req.body;
-
-  const [existingUser] = await db.select({id:usersTable.id}).from(usersTable).where(eq(usersTable.email.email));
-})
-
-  if(existingUser){res.status(400).json({erro:"Email already exist"}) }
+router.post("/signup", async (req, res) => {
+  const validationResult = await signupPostRequestBodySchema.safeParseAsync(
+    req.body
+  );
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.format() });
+  }
+  console.log(validationResult.data);
+  const { firstName, lastName, email, password } = validationResult.data;
+  const [existingEmail] = await db
+    .select({ email: usersTable.email })
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+  if (existingEmail) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+  const salt = randomBytes(256).toString("hex");
+  const hashedPass = createHmac("sha256", salt).update(password).digest("hex");
+  const [insertUser] = await db
+    .insert(usersTable)
+    .values({ firstName, lastName, email, password: hashedPass, salt })
+    .returning({ id: usersTable.id });
+  return res.status(200).json({ data: { userID: insertUser.id } });
+});
 
 export default router;
